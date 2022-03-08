b2fbf52bbb3aaa12f0497bace165a59c949ecad4
Paulo de Tarso <paulo.oinab@gmail.com>
2022-03-08 19:06:05 -0300

    Ordem crescente com bubble sort method

---
+++ fundamentos/bloco-04-introducao-javascript-e-logica/dia-02-javascript-array-for/bubbleSort.js
@@ -0,0 +1,14 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//Ordem crescente com bubble sort method
+for(i = 0; i < numbers.length; i += 1) {
+    for(j = 0; j < numbers.length - i - 1; j += 1) {
+        if(numbers[j] > numbers[j+1]) {
+            let pos = numbers[j];
+            numbers[j] = numbers[j+1];
+            numbers[j+1] = pos;
+        }
+    }
+}
+
+console.log(numbers);
