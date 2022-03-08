c0a0e9e58815c09de9cb36752a9f9546661df2f8
Paulo de Tarso <paulo.oinab@gmail.com>
2022-03-08 18:04:20 -0300

    Soma todos os elementos do array

--- fundamentos/bloco-04-introducao-javascript-e-logica/dia-02-javascript-array-for/script.js
+++ fundamentos/bloco-04-introducao-javascript-e-logica/dia-02-javascript-array-for/script.js
@@ -1,5 +1,13 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
+//Imprime todos os elementos do array
 for(let i = 0; i < numbers.length; i += 1) {
     console.log(numbers[i]);
 }
+
+//Soma todos os elementos do array
+let soma = 0;
+for(let i = 0; i < numbers.length; i += 1) {
+    soma += numbers[i];
+}
+console.log(soma);
