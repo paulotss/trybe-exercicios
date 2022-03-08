8651036a82c289dbca1fa5e3a41aac4f28f510cd
Paulo de Tarso <paulo.oinab@gmail.com>
2022-03-08 19:09:30 -0300

    Ordem decrescente com bubble sort

--- fundamentos/bloco-04-introducao-javascript-e-logica/dia-02-javascript-array-for/bubbleSort.js
+++ fundamentos/bloco-04-introducao-javascript-e-logica/dia-02-javascript-array-for/bubbleSort.js
@@ -12,3 +12,16 @@ for(i = 0; i < numbers.length; i += 1) {
 }
 
 console.log(numbers);
+
+//Ordem decrescente com bubble sort
+for(i = 0; i < numbers.length; i += 1) {
+    for(j = 0; j < numbers.length - i - 1; j += 1) {
+        if(numbers[j] < numbers[j+1]) {
+            let pos = numbers[j];
+            numbers[j] = numbers[j+1];
+            numbers[j+1] = pos;
+        }
+    }
+}
+
+console.log(numbers);
