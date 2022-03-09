120c1bdd93d3658181665f80af4aa6850b69ddc7
Paulo de Tarso <paulo.oinab@gmail.com>
2022-03-09 19:54:29 -0300

    Retorna maior e menor palavra em um array (bubble sort)

--- fundamentos/bloco-04-introducao-javascript-e-logica/dia-03-javascript-logica-programacao-algoritmos/script.js
+++ fundamentos/bloco-04-introducao-javascript-e-logica/dia-03-javascript-logica-programacao-algoritmos/script.js
@@ -16,3 +16,30 @@ for(let i = word.length - 1; i >= 0; i -= 1) {
 }
 
 console.log(invertWord);
+
+//Retorna maior e menor palavra em um array (bubble sort)
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let teste = '';
+
+for (let i = 0; i < array.length; i += 1) {
+
+    for(let j = 0; j < array.length - i - 1; j += 1) {
+
+        if(array[j + 1].length > array[j].length) {
+
+            let aux = array[j];
+            array[j] = array[j + 1];
+            array[j + 1] = aux;
+
+        }
+
+        teste += '|' + array[j];
+
+    }
+
+    console.log(teste);
+    teste = '';
+}
+
+console.log("Maior palavra é " + array[0]);
+console.log("Menor palavra é " + array[array.length - 1]);
