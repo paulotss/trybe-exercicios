0fb16c7daf3a6f0ff30c92bb39ca2eadafb0ae87
Paulo de Tarso <paulo.oinab@gmail.com>
2022-03-09 20:49:44 -0300

    Bônus - Triângulo invertido com base n

--- fundamentos/bloco-04-introducao-javascript-e-logica/dia-03-javascript-logica-programacao-algoritmos/bonus.js
+++ fundamentos/bloco-04-introducao-javascript-e-logica/dia-03-javascript-logica-programacao-algoritmos/bonus.js
@@ -19,3 +19,18 @@ for(let i = 0; i < t; i += 1) {
     console.log(linha);
     linha = '';
 }
+
+//Bônus - Triângulo invertudo com base n
+let it = 10;
+linha = '';
+for (let i = it; i > 0; i -= 1) {
+    for (let j = 1; j <= 10; j += 1) {
+        if(j >= i) {
+            linha += '*';
+        } else {
+            linha += ' ';
+        }
+    }
+    console.log(linha);
+    linha = '';
+}
