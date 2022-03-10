866170808c92201fee073eaf76e6892112421293
Paulo de Tarso <paulo.oinab@gmail.com>
2022-03-10 19:23:24 -0300

    Cria function que retorna maior string em um array

--- fundamentos/bloco-04-introducao-javascript-e-logica/dia-04-javascript-objetos-funcoes/scriptFunctions.js
+++ fundamentos/bloco-04-introducao-javascript-e-logica/dia-04-javascript-objetos-funcoes/scriptFunctions.js
@@ -37,3 +37,37 @@ function maiorValor(arr) {
 arrTeste = [2, 3, 6, 7, 10, 1];
 
 console.log(maiorValor(arrTeste));
+
+function menorValor(arr) {
+    let test;
+
+    for(let key in arr) {
+        for(let val of arr) {
+            if(arr[key] <= val) {
+                test = true;
+            } else {
+                test = false;
+                break;
+            }
+        }
+        if(test) {
+            return key;
+        }
+    }
+}
+
+arrTeste = [2, 4, 6, 7, 10, 0, -3];
+
+console.log(menorValor(arrTeste));
+
+function maiorNome(arr) {
+    let keys = []
+    for(let val of arr) {
+        keys.push(val.length);
+    }
+    return arr[maiorValor(keys)];
+}
+
+let nomes = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+
+console.log(maiorNome(nomes));
