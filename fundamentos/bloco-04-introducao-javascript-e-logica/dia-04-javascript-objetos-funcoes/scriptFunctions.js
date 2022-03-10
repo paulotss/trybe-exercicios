0fa2b2408c612f1053a34ee8264492a9b9e8939e
Paulo de Tarso <paulo.oinab@gmail.com>
2022-03-10 19:12:38 -0300

    Retorna chave maior elemento array

--- fundamentos/bloco-04-introducao-javascript-e-logica/dia-04-javascript-objetos-funcoes/scriptFunctions.js
+++ fundamentos/bloco-04-introducao-javascript-e-logica/dia-04-javascript-objetos-funcoes/scriptFunctions.js
@@ -15,3 +15,25 @@ function verificaPalindromo(word) {
 }
 
 console.log(verificaPalindromo('desenvolvimento'));
+
+function maiorValor(arr) {
+    let test;
+
+    for(let key in arr) {
+        for(let val of arr) {
+            if(arr[key] >= val) {
+                test = true;
+            } else {
+                test = false;
+                break;
+            }
+        }
+        if(test) {
+            return key;
+        }
+    }
+}
+
+arrTeste = [2, 3, 6, 7, 10, 1];
+
+console.log(maiorValor(arrTeste));
