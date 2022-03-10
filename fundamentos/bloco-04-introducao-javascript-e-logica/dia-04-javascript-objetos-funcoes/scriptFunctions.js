7c097d26c98831c1460a76cd329e17eed92ea920
Paulo de Tarso <paulo.oinab@gmail.com>
2022-03-10 19:38:57 -0300

    Soma numero natural

--- fundamentos/bloco-04-introducao-javascript-e-logica/dia-04-javascript-objetos-funcoes/scriptFunctions.js
+++ fundamentos/bloco-04-introducao-javascript-e-logica/dia-04-javascript-objetos-funcoes/scriptFunctions.js
@@ -94,3 +94,13 @@ function maisRepete(arr) {
 arrTeste2 = [2, 3, 2, 5, 8, 2, 3];
 
 console.log(maisRepete(arrTeste2));
+
+function somaArray(num) {
+    let sum = 0;
+    for(let i = 1; i <= num; i += 1) {
+        sum += i;
+    }
+    return sum;
+}
+
+console.log(somaArray(5));
