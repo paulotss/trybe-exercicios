24bb5e24b46eb7c81dea076c9262f4f034a77254
Paulo de Tarso <paulo.oinab@gmail.com>
2022-03-10 18:27:06 -0300

    Imprime 2 objetos em um unico for/in

--- fundamentos/bloco-04-introducao-javascript-e-logica/dia-04-javascript-objetos-funcoes/script.js
+++ fundamentos/bloco-04-introducao-javascript-e-logica/dia-04-javascript-objetos-funcoes/script.js
@@ -4,6 +4,13 @@ let info = {
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
 
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: 'Christmas on Bear Mountain, Dell\'s Four Color Comics #178',
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim'
+};
+
 //001
 console.log('Bem vinda ' + info.personagem);
 
@@ -19,4 +26,28 @@ for(let key in info) {
 //004
 for(let key in info) {
     console.log(info[key]);
+}
+
+//005
+for(let key in info) {
+    switch(key) {
+        case 'personagem' :
+            console.log(info[key] + ' e ' + info2[key]);
+            break;
+        case 'origem' :
+            console.log(info[key] + ' e ' + info2[key]);
+            break;
+        case 'nota' :
+            console.log(info[key] + ' e ' + info2[key]);
+            break;
+        case 'recorrente' : 
+            if(info[key] === 'Sim' && info2[key] === 'Sim') {
+                console.log('Ambos recorrentes');
+            } else {
+                console.log(info[key] + ' e ' + info2[key]);
+            }
+            break;
+        default :
+            console.log('Erro');
+    }
 }
