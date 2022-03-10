0442c19533e951e3bba7501c6b109b53fd2f0e87
Paulo de Tarso <paulo.oinab@gmail.com>
2022-03-10 18:30:50 -0300

    Acesso array em um objeto

--- fundamentos/bloco-04-introducao-javascript-e-logica/dia-04-javascript-objetos-funcoes/script.js
+++ fundamentos/bloco-04-introducao-javascript-e-logica/dia-04-javascript-objetos-funcoes/script.js
@@ -51,3 +51,19 @@ for(let key in info) {
             console.log('Erro');
     }
 }
+
+//006
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+};
+
+console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama ' + leitor.livrosFavoritos[0].titulo);
