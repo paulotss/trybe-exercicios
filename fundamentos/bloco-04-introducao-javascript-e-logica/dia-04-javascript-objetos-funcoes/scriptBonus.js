eef5e0c6730591095929e0e26963d01def0c03c2
Paulo de Tarso <paulo.oinab@gmail.com>
2022-03-11 11:42:40 -0300

    Function get even numbers of a vector

--- fundamentos/bloco-04-introducao-javascript-e-logica/dia-04-javascript-objetos-funcoes/scriptBonus.js
+++ fundamentos/bloco-04-introducao-javascript-e-logica/dia-04-javascript-objetos-funcoes/scriptBonus.js
@@ -53,3 +53,18 @@ function converteEmRomanos(str) {
 }
 
 console.log(converteEmRomanos('IV'));
+
+function arrayOfNumbers(vector) {
+    const pares = [];
+    for(let i = 0; i < vector.length; i += 1) {
+        for(let j = 0; j < vector[i].length; j += 1) {
+            if(vector[i][j] % 2 === 0) {
+                pares.push(vector[i][j]);
+            }
+        }
+    }
+    return pares;
+}
+
+let vector = [[1, 2], [3,4,5,6], [7,8,9,10]];
+console.log(arrayOfNumbers(vector));
