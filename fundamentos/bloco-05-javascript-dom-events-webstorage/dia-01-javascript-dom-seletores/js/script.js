e7cad3084417ec5b237b52441dd7e82939e68349
Paulo de Tarso <paulo.oinab@gmail.com>
2022-03-14 15:49:58 -0300

    Adiciona cores para elementos usando DOM

---
+++ fundamentos/bloco-05-javascript-dom-events-webstorage/dia-01-javascript-dom-seletores/js/script.js
@@ -0,0 +1,25 @@
+let header = document.getElementById('header-container');
+header.style.backgroundColor = "green";
+
+let emergency = document.querySelectorAll('.emergency-tasks div');
+for(let val of emergency) {
+    val.style.backgroundColor = 'pink';
+}
+
+let noEmergency = document.querySelectorAll('.no-emergency-tasks div');
+for(let val of noEmergency) {
+    val.style.backgroundColor = 'yellow';
+}
+
+let emergencyH3 = document.querySelectorAll('.emergency-tasks div h3');
+for(let val of emergencyH3) {
+    val.style.backgroundColor = '#A334A4';
+}
+
+let noEmergencyH3 = document.querySelectorAll('.no-emergency-tasks div h3');
+for(let val of noEmergencyH3) {
+    val.style.backgroundColor = 'black';
+}
+
+let footer = document.querySelector('#footer-container div');
+footer.style.backgroundColor = '#074242';
