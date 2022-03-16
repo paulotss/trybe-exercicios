dfb6be1212d9d0c11d95ec1b5d6c55e2c40ea6a1
Paulo de Tarso <paulo.oinab@gmail.com>
2022-03-16 18:30:21 -0300

    Exercicio 1

--- fundamentos/bloco-05-javascript-dom-events-webstorage/dia-03-javascript-eventos/exercicios/script.js
+++ fundamentos/bloco-05-javascript-dom-events-webstorage/dia-03-javascript-eventos/exercicios/script.js
@@ -14,3 +14,39 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+const ulDays = document.getElementById('days');
+
+function createDayDez(arr) {
+  let li;
+  for(let val of arr) {
+    li = document.createElement('li');
+    li.classList.add('day');
+    li.innerText = val;
+    switch(val) {
+      case 24:
+        li.classList.add('holiday');
+        break;
+      case 25:
+        li.classList.add('holiday');
+        li.classList.add('friday');
+        break;
+      case 31:
+        li.classList.add('holiday');
+        break;
+      case 4:
+        li.classList.add('friday');
+        break;
+      case 11:
+        li.classList.add('friday');
+        break;
+      case 18:
+        li.classList.add('friday');
+        break;
+    }
+    ulDays.appendChild(li);
+  }
+}
+
+createDayDez(dezDaysList);
