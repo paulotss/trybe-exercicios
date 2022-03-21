c1e2fb07335a11cb76d9519a968954c9cb8837a2
Paulo de Tarso <paulo.oinab@gmail.com>
2022-03-21 14:23:37 -0300

    Adiciona no local storage background e text color

--- fundamentos/bloco-05-javascript-dom-events-webstorage/dia-04-javascript-webstorage/script.js
+++ fundamentos/bloco-05-javascript-dom-events-webstorage/dia-04-javascript-webstorage/script.js
@@ -1,21 +1,36 @@
 "use strict";
 
+const body = document.querySelector('body');
+const article = document.querySelector('article');
+
+body.style.backgroundColor = localStorage.getItem('background') ? localStorage.getItem('background') : 'white';
+document.getElementById('background').value = localStorage.getItem('background') ? localStorage.getItem('background') : 'white';
+
+body.style.color = localStorage.getItem('text-color') ? localStorage.getItem('text-color') : 'black';
+document.getElementById('text-color').value = localStorage.getItem('text-color') ? localStorage.getItem('text-color') : 'black';
+
+// article.style.fontSize = e.target.value;
+// article.style.lineHeight = e.target.value;
+// body.style.fontFamily = e.target.value;
+
 document.getElementById('background').addEventListener('change', function(e) {
-  document.querySelector('body').style.backgroundColor = e.target.value;
+  localStorage.setItem('background', e.target.value);
+  body.style.backgroundColor = e.target.value;
 });
 
 document.getElementById('text-color').addEventListener('change', function(e) {
-  document.querySelector('body').style.color = e.target.value;
+  localStorage.setItem('text-color', e.target.value);
+  body.style.color = e.target.value;
 });
 
 document.getElementById('font-size').addEventListener('change', function(e) {
-  document.querySelector('article').style.fontSize = e.target.value;
+  article.style.fontSize = e.target.value;
 });
 
 document.getElementById('line-height').addEventListener('change', function(e) {
-  document.querySelector('article').style.lineHeight = e.target.value;
+  article.style.lineHeight = e.target.value;
 });
 
 document.getElementById('font-family').addEventListener('change', function(e) {
-  document.querySelector('body').style.fontFamily = e.target.value;
+  body.style.fontFamily = e.target.value;
 });
