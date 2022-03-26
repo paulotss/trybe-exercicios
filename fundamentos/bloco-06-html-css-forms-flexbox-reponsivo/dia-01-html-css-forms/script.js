1faa4c7ed12d6a704063c92415c2b3fde3bbc308
Paulo de Tarso <paulo.oinab@gmail.com>
2022-03-26 13:33:19 -0300

    Adiciona alert para dados invalidos

--- fundamentos/bloco-06-html-css-forms-flexbox-reponsivo/dia-01-html-css-forms/script.js
+++ fundamentos/bloco-06-html-css-forms-flexbox-reponsivo/dia-01-html-css-forms/script.js
@@ -1,3 +1,10 @@
 document.getElementById('send').addEventListener('click', function(e) {
   e.preventDefault();
+  let check = document.getElementById('photo-agree-id').checked;
+  console.log(check);
+  if(!check) {
+    alert('Dados inválidos!');
+  } else {
+    alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.');
+  }
 });
