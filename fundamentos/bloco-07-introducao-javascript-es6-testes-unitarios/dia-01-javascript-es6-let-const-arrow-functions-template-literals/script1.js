f42b8a1eb62a7912733e117274cc935159c0c829
Paulo de Tarso <paulo.oinab@gmail.com>
2022-04-04 23:27:10 -0300

    Muda string utilizando template literals

--- fundamentos/bloco-07-introducao-javascript-es6-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-template-literals/script1.js
+++ fundamentos/bloco-07-introducao-javascript-es6-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-template-literals/script1.js
@@ -1,10 +1,10 @@
 testingScope = escopo => {
   if (escopo === true) {
-    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+    let ifScope = `Não devo ser utilizada fora do meu escopo (if)`;
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
     return ifScope;
   } else {
-    let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    let elseScope = `Não devo ser utilizada fora meu escopo (else)`;
     return elseScope;
   }
 }
