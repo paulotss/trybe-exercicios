36e8a906b1797e4d37cc7960ed331cf0e949feae
Paulo de Tarso <paulo.oinab@gmail.com>
2022-04-05 14:08:56 -0300

    Coloca array em ordem crescente

--- fundamentos/bloco-07-introducao-javascript-es6-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-template-literals/script1.js
+++ fundamentos/bloco-07-introducao-javascript-es6-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-template-literals/script1.js
@@ -10,3 +10,18 @@ testingScope = escopo => {
 }
 
 console.log(testingScope(true));
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+let aux = 0;
+
+for (let i = 0; i < oddsAndEvens.length; i += 1) {
+  for (let j = 0; j < (oddsAndEvens.length - i); j += 1) {
+    if(oddsAndEvens[j] < oddsAndEvens[j - 1]) {
+      aux = oddsAndEvens[j];
+      oddsAndEvens[j] = oddsAndEvens[j - 1];
+      oddsAndEvens[j - 1] = aux;
+    }
+  }
+}
+
+console.log(oddsAndEvens); // será necessário alterar essa linha 😉
