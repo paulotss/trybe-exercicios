ade15d3d547de941be0c32eb13aff17f84136a75
Paulo de Tarso <paulo.oinab@gmail.com>
2022-04-06 12:43:24 -0300

    A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array sem o elemento item caso ele exista no array

--- fundamentos/bloco-07-introducao-javascript-es6-testes-unitarios/dia-03-primeiros-passos-jest/sum.test.js
+++ fundamentos/bloco-07-introducao-javascript-es6-testes-unitarios/dia-03-primeiros-passos-jest/sum.test.js
@@ -1,4 +1,4 @@
-const sum = require('./sum');
+const {sum, myRemove} = require('./sum');
 
 describe('Requisito 1', () => {
   it('Teste se o retorno de sum(4, 5) é 9', () => {
@@ -14,3 +14,18 @@ describe('Requisito 1', () => {
     expect(() => {sum(4, '5')}).toThrow('parameters must be numbers');
   })
 });
+
+describe('Requisito 2', () => {
+  it('Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado', () => {
+    const arr = [1, 2, 3, 4];
+    expect(myRemove(arr, 3)).toEqual([1, 2, 4]);
+  });
+  it('Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
+    const arr = [1, 2, 3, 4];
+    expect(myRemove(arr, 3)).not.toEqual(arr);
+  });
+  it('Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado', () => {
+    const arr = [1, 2, 3, 4];
+    expect(myRemove(arr, 5)).toEqual(arr);
+  })
+})
