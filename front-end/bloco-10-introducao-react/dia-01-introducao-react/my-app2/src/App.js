b8d82b5d33b97872027dcac9f204c970065ef4cc
Paulo de Tarso <paulo.oinab@gmail.com>
2022-05-06 15:43:13 -0300

    Renderize o componente Footer no app.js

--- front-end/bloco-10-introducao-react/dia-01-introducao-react/my-app2/src/App.js
+++ front-end/bloco-10-introducao-react/dia-01-introducao-react/my-app2/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Header.jsx';
 import Content from './Content.jsx';
+import Footer from './Footer';
 
 function App() {
   return (
     <div>
       <Header />
       <Content />
+      <Footer />
     </div>
   );
 }
