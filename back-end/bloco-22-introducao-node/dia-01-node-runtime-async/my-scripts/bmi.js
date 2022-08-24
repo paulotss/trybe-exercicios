e62b9fe3a6cc89f3050c038d053feac98158d232
Paulo de Tarso <paulo.oinab@gmail.com>
2022-08-24 14:46:13 -0300

    Exercícios 4 e 5

--- back-end/bloco-22-introducao-node/dia-01-node-runtime-async/my-scripts/bmi.js
+++ back-end/bloco-22-introducao-node/dia-01-node-runtime-async/my-scripts/bmi.js
@@ -8,4 +8,22 @@ const bmi = (weight, height) => {
 const weight = readline.questionFloat("What' your weight? ");
 const height = readline.questionFloat("What' your height? ");
 
-console.log(bmi(weight, height));
+const result = bmi(weight, height);
+
+if(result < 18.5) {
+  console.log(`${result} Abaixo do peso (magreza)`);
+} else if (result < 24.9 && result >= 18.5) {
+  console.log(`${result} Peso normal`);
+} else if (result < 29.9 && result >= 25) {
+  console.log(`${result} Acima do peso (sobrepeso)`);
+} else if (result < 34.9 && result >= 30) {
+  console.log(`${result} Obesidade grau I`);
+} else if (result < 39.9 && result >= 35) {
+  console.log(`${result} Obesidade grau II`);
+} else if (result > 40) {
+  console.log(`${result} Obesidade graus III e IV`);
+} else {
+  console.log('Inderteminado.');
+}
+
+//console.log(bmi(weight, height));
