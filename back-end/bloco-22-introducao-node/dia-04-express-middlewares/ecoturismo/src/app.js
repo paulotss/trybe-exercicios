30c1debcfdfc74201498b927173b05b7e210412f
Paulo de Tarso <paulo.oinab@gmail.com>
2022-08-28 23:27:06 -0300

    Exercícios 1, 2 e 3

--- back-end/bloco-22-introducao-node/dia-04-express-middlewares/ecoturismo/src/app.js
+++ back-end/bloco-22-introducao-node/dia-04-express-middlewares/ecoturismo/src/app.js
@@ -1,4 +1,18 @@
 const express = require('express');
+const fs = require('fs/promises');
+const { join } = require('path');
+require('express-async-errors');
+
 const app = express();
+
+app.use(express.json());
+
+app.post('/activities', async (req, res) => {
+  const contentFile = await fs.readFile(join(__dirname, './files/ecoturismo.json'), 'utf-8');
+  const activities = JSON.parse(contentFile);
+  activities.push(req.body);
+  await fs.writeFile(join(__dirname, './files/ecoturismo.json'), JSON.stringify(activities));
+  res.status(201).json({ "message": "Atividade cadastrada com sucesso!" });
+});
 
 module.exports = app;
