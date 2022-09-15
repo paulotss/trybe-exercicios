28df48d4de84b245a20b4ce309bd4c35adece400
Paulo de Tarso <paulo.oinab@gmail.com>
2022-09-15 19:20:25 -0300

    Exercício 7

--- back-end/secao-06-node-orm-autenticacao/dia-01-orm/src/controllers/book.controller.js
+++ back-end/secao-06-node-orm-autenticacao/dia-01-orm/src/controllers/book.controller.js
@@ -5,6 +5,14 @@ const getAll = async (_req, res) => {
   res.status(200).json(result);
 };
 
+const getById = async (req, res) => {
+  const { id } = req.params;
+  const result = await bookService.getById(id);
+  if(result.length < 1) return res.status(404).json({ message: 'Book not found' });
+  res.status(200).json(result);
+}
+
 module.exports = {
   getAll,
+  getById,
 };
