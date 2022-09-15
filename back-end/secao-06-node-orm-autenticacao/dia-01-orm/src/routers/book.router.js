bcdc1bd0ca35f085e4b7287d18b0899513bd0783
Paulo de Tarso <paulo.oinab@gmail.com>
2022-09-15 19:28:31 -0300

    Exercício 11

--- back-end/secao-06-node-orm-autenticacao/dia-01-orm/src/routers/book.router.js
+++ back-end/secao-06-node-orm-autenticacao/dia-01-orm/src/routers/book.router.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.get('/', bookController.getAll);
 router.get('/:id', bookController.getById);
+router.post('/', bookController.create);
 
 module.exports = router;
