21afdd2f7b57ba541e7ebb26a1a2cb886f8d524b
Paulo de Tarso <paulo.oinab@gmail.com>
2022-10-10 22:54:30 -0300

    Exercício 02

--- back-end/secao-09-poo-solid/dia-01-introducao-poo/index.ts
+++ back-end/secao-09-poo-solid/dia-01-introducao-poo/index.ts
@@ -1,14 +1,36 @@
 class Student {
   id: number;
   name: string;
-  noteJob: number[] = [];
-  noteTest: number[] = [];
+  private _noteJob: number[] = [];
+  private _noteTest: number[] = [];
 
   constructor(id: number, name: string) {
     this.id = id;
     this.name = name;
   }
+
+  set noteJob(note: number) {
+    this._noteJob.push(note);
+  }
+
+  set noteTest(note: number) {
+    this._noteTest.push(note);
+  }
+
+  sumNotes() {
+    const noteJob = this._noteJob.reduce((acc, val) =>  acc += val);
+    const noteTest = this._noteTest.reduce((acc, val) => acc += val);
+    return noteJob + noteTest;
+  }
+
+  averageNote() {
+    const numNotes = this._noteJob.length + this._noteTest.length;
+    return this.sumNotes() / numNotes;
+  }
 }
 
 const test = new Student(123, 'Fulano');
-console.log(test);
+test.noteJob = 10;
+test.noteTest = 10;
+console.log(test.sumNotes());
+console.log(test.averageNote());
